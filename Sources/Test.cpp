a59d7672c52278a3e940b4b5c24e7b10749dc2ee
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2025-10-26 00:33:02 +0900

    Add Test

---
+++ Sources/Test.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+
+#include <iostream>
+
+namespace Pastel
+{
+    void Test::DoSomething() noexcept
+    {
+        std::cout << "Doing something!" << std::endl;
+    }
+}
---
+++ Sources/Test.h
@@ -0,0 +1,13 @@
+#ifndef PASTEL_TEST_H
+#define PASTEL_TEST_H
+
+namespace Pastel
+{
+    class Test
+    {
+    public:
+        static void DoSomething() noexcept;
+    };
+}
+
+#endif
